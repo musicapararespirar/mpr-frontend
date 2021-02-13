83035335db4d5161b7bb3dac95b7e8c501925343
Evan Thomas <evan@evanjt.com>
2021-02-13 15:56:21 +1100

    End of 3.6

---
+++ config/db.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get("MongoURI");
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+
+        });
+
+        console.log("MongoDB connected.");
+    } catch(err) {
+        console.error(err.message);
+        // Failure on exit
+        process.exit(1);
+    }
+}
+
+
+module.exports = connectDB;
