bcf614235de4450c26a86db5b5c1f6d34a24f587
Evan Thomas <evan@evanjt.com>
2021-02-14 21:54:15 +1100

    End of 6.4

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 // import logo from './logo.svg';
 import React, { Fragment } from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+
 
 const App = () => (
     <Fragment>
-        <h1>App</h1>
+        <Navbar />
+        <Landing />
     </Fragment>
 );
 export default App;
