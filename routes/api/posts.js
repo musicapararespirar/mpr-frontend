f8b0ff6ee11e6bb064c03dd766bb59471075eb8e
Evan Thomas <evan@evanjt.com>
2021-02-14 19:15:45 +1100

    Middle of 5.2

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const { chec, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
 
-// @route  GET api/posts
-// @desc   Test route
-// @access Public
-router.get('/', (req, res) => res.send('Posts route'));
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+
+// @route  POST api/posts
+// @desc   Create a post
+// @access Private
+router.post(
+    '/',
+    [
+        auth,
+        [
+            check('text', 'Text is required').not().isEmpty()
+        ]
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const user = await User
+    });
 
 module.exports = router;
