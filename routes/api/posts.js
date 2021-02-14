429c7be62f6937a2b116d26cb70d2ce143c41bb3
Evan Thomas <evan@evanjt.com>
2021-02-14 19:29:52 +1100

    End of 5.2

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { chec, validationResult } = require('express-validator');
+const { check, validationResult } = require('express-validator');
 const auth = require('../../middleware/auth');
 
 const Post = require('../../models/Post');
@@ -25,7 +25,24 @@ router.post(
             return res.status(400).json({ errors: errors.array() });
         }
 
-        const user = await User
+        try {
+            const user = await User.findById(req.user.id).select('-password');
+
+            const newPost = new Post({
+                text: req.body.text,
+                name: user.name,
+                avatar: user.avatar,
+                user: req.user.id
+            })
+
+            const post = await newPost.save();
+
+            res.json(post); // Once get post, add it to the response
+        } catch(err) {
+            console.error(err.message);
+            res.status(500).send("Server error");
+        }
+
     });
 
 module.exports = router;
