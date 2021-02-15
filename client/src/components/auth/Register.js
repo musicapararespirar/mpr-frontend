fc205fe9036d68c1520fcef464f54bc3fcc2ac28
Evan Thomas <evan@evanjt.com>
2021-02-15 22:10:43 +1100

    Setting up alerts in redux

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,8 +1,10 @@
 import React, { Fragment, useState } from 'react'
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { setAlert } from '../../actions/alert';
 // import axios from 'axios';
 
-const Register = () => {
+const Register = (props) => {
      const [formData, setFormData] = useState({
          name: '',
          email: '',
@@ -16,7 +18,7 @@ const Register = () => {
     const onSubmit = async e => {
         e.preventDefault();
         if(password !== password2) {
-            console.log('Passwords do not match');
+            props.setAlert('Passwords do not match', 'danger');
         } else {
             console.log('SUCCESS');
 //             const newUser = {
@@ -96,4 +98,4 @@ const Register = () => {
      )
 }
 
-export default Register
+export default connect(null, { setAlert })(Register);
