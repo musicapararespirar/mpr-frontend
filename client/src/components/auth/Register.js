156e756afe9da94d6d4dfed782467de632bf535a
Evan Thomas <evan@evanjt.com>
2021-02-15 22:32:10 +1100

    End of 7 (redux)

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,9 +2,10 @@ import React, { Fragment, useState } from 'react'
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
 // import axios from 'axios';
 
-const Register = (props) => {
+const Register = ({ setAlert }) => {
      const [formData, setFormData] = useState({
          name: '',
          email: '',
@@ -18,7 +19,7 @@ const Register = (props) => {
     const onSubmit = async e => {
         e.preventDefault();
         if(password !== password2) {
-            props.setAlert('Passwords do not match', 'danger');
+            setAlert('Passwords do not match', 'danger');
         } else {
             console.log('SUCCESS');
 //             const newUser = {
@@ -96,6 +97,10 @@ const Register = (props) => {
       </p>
       </Fragment>
      )
+}
+
+Register.propTypes = {
+    setAlert: PropTypes.func.isRequired
 }
 
 export default connect(null, { setAlert })(Register);
