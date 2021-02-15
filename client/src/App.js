2a870ad689b8d50a693b766a6f2925ba4856a5ac
Evan Thomas <evan@evanjt.com>
2021-02-15 21:27:54 +1100

    Setup redux store

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,30 @@
 // import logo from './logo.svg';
 import React, { Fragment } from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import './App.css';
 
+// Redux imports
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => (
+    <Provider store={store}>
+    <Router>
     <Fragment>
         <Navbar />
-        <Landing />
-    </Fragment>
+        <Route exact path="/" component={Landing} />
+        <section className="container">
+            <Switch>
+                <Route exact path="/register" component={Register} />
+                <Route exact path="/login" component={Login} />
+            </Switch>
+        </section>
+        </Fragment>
+    </Router>
+    </Provider>
 );
 export default App;
