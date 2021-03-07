6a9722645d738bbf2cca989b5067ba73149cb0fa
Evan Thomas <evan@evanjt.com>
2021-03-08 08:09:11 +1100

    Finish app

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,9 +1,12 @@
 import {
     GET_POSTS,
+    GET_POST,
     POST_ERROR,
     UPDATE_LIKES,
     DELETE_POST,
-    ADD_POST
+    ADD_POST,
+    ADD_COMMENT,
+    REMOVE_COMMENT
 } from '../actions/types';
 
 const initialState = {
@@ -23,6 +26,12 @@ export default function(state = initialState, action) {
                 posts: payload,
                 loading: false
             };
+        case GET_POST:
+            return {
+                ...state,
+                post: payload,
+                loading: false
+            };
         case ADD_POST:
             return {
                 ...state,
@@ -31,13 +40,13 @@ export default function(state = initialState, action) {
                 ...state.posts
                 ],
                 loading: false
-            }
+            };
         case DELETE_POST:
             return {
                 ...state,
                 posts: state.posts.filter(post => post._id !== payload),
                 loading: false
-            }
+            };
         case POST_ERROR:
             return {
                 ...state,
@@ -50,6 +59,26 @@ export default function(state = initialState, action) {
                 posts: state.posts.map(post => post._id === payload.id ? {
                     ...post,
                     likes: payload.likes } : post),
+                loading: false
+            };
+        case ADD_COMMENT:
+            return {
+                ...state,
+                post: {
+                    ...state.post,
+                    comments: payload
+                },
+                loading: false
+            }
+        case REMOVE_COMMENT:
+            return {
+                ...state,
+                post: {
+                    ...state.post,
+                    comments: state.post.comments.filter(
+                        comment => comment._id !== payload
+                    )
+                },
                 loading: false
             }
         default:
