12abf09daefa7fafcaaa815f0050016e4d0db4f5
Evan Thomas <evan@evanjt.com>
2021-04-25 20:00:24 +1000

    Change menu

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,52 +3,163 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { logout } from '../../actions/auth';
-
+import NavbarDropdown from 'react-navbar-dropdown';
 const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
-    const authLinks = (
-        <ul>
-            {/*<li><Link to="/about-us">About Us</Link></li>*/}
-            {/*<li><Link to="/profiles">Musicians</Link></li>*/}
-            {/*<li><Link to="/about-us">Sobre</Link></li>*/}
-            {/*<li><Link to="/concerts">Conciertos</Link></li>*/}
-            <li><Link to="/impact">Impacto</Link></li>
-            <li><Link to="/contribute">Contribuye</Link></li>
-            <li><Link to="/contact">Contáctanos</Link></li>
-            <li><Link to="/posts">Posts</Link></li>
-            <li>
-                <Link to="/dashboard">
-                    <i className="fas fa-tachometer-alt" />
-                </Link>
-            </li>
+//     const authLinks = (
+//         <ul>
+//             {/*<li><Link to="/about-us">About Us</Link></li>*/}
+//             {/*<li><Link to="/profiles">Musicians</Link></li>*/}
+//             {/*<li><Link to="/about-us">Sobre</Link></li>*/}
+//             {/*<li><Link to="/concerts">Conciertos</Link></li>*/}
+//             <li><Link to="/impact">Impacto</Link></li>
+//
+//
+//
+//             <li><Link to="/contribute">Contribuye</Link></li>
+//
+//
+//
+//             <li><Link to="/contact">Contáctanos</Link></li>
+//
+//
+//
+//             <li><Link to="/posts">Posts</Link></li>
+//
+//
+//
+//             <li>
+//                 <Link to="/dashboard">
+//                     <i className="fas fa-tachometer-alt" />
+//                 </Link>
+//             </li>
+//
+//             <li>
+//                 <a onClick={logout} href="#!">
+//                     <i className="fas fa-sign-out-alt" />{' '}
+//                 </a>
+//             </li>
+//         </ul>
+//     );
 
-            <li>
-                <a onClick={logout} href="#!">
-                    <i className="fas fa-sign-out-alt" />{' '}
-                </a>
-            </li>
-        </ul>
+        const guestLinks = (
+            <NavbarDropdown>
+        <NavbarDropdown.Toggle className="menu__item">
+        <NavbarDropdown.Open>
+            <i className="fas fa-bars fa-3x" />
+        </NavbarDropdown.Open>
+        <NavbarDropdown.Close>
+            <i className="fa fa-times fa-3x" />
+        </NavbarDropdown.Close>
+        </NavbarDropdown.Toggle>
+        <NavbarDropdown.Menu className="navmenu-menu">
+            <div className="navmenu-menu__row">
+                <Link to='/about-us'>
+                    <NavbarDropdown.Item className="navmenu-item">
+                        <div className="navmenu-item__icon"><i className="fas fa-users" /></div>
+                        <div className="navmenu-item__text">Sobre</div>
+                    </NavbarDropdown.Item>
+                </Link>
+                <Link to='/concerts'>
+                    <NavbarDropdown.Item className="navmenu-item">
+                        <div className="navmenu-item__icon"><i className="fas fa-guitar" /></div>
+                        <div className="navmenu-item__text">Conciertos</div>
+                    </NavbarDropdown.Item>
+                </Link>
+            </div>
+            <div className="navmenu-menu__row">
+                <Link to='/impact'>
+                    <NavbarDropdown.Item className="navmenu-item">
+                        <div className="navmenu-item__icon"><i className="fas fa-hands-helping" /></div>
+                        <div className="navmenu-item__text">Impacto</div>
+                    </NavbarDropdown.Item>
+                </Link>
+            </div>
+            <div className="navmenu-menu__row">
+                <Link to='/contribute'>
+                    <NavbarDropdown.Item className="navmenu-item">
+                        <div className="navmenu-item__icon"><i className="fas fa-donate" /></div>
+                        <div className="navmenu-item__text">Contribuye</div>
+                    </NavbarDropdown.Item>
+                </Link>
+                <Link to='/contact'>
+                    <NavbarDropdown.Item className="navmenu-item">
+                        <div className="navmenu-item__icon"><i className="fas fa-envelope" /></div>
+                        <div className="navmenu-item__text">Contáctanos</div>
+                    </NavbarDropdown.Item>
+                </Link>
+            </div>
+        </NavbarDropdown.Menu>
+        </NavbarDropdown>
     );
 
-    const guestLinks = (
-        <ul>
-            <li><Link to="/about-us">Sobre</Link></li>
-            <li><Link to="/concerts">Conciertos</Link></li>
-            <li><Link to="/impact">Impacto</Link></li>
-            <li><Link to="/contribute">Contribuye</Link></li>
-            <li><Link to="/contact">Contáctanos</Link></li>
-            {/*<li><Link to="/profiles">Musicians</Link></li>*/}
-            {/*<li><Link to="/register">Register</Link></li>*/}
-            {/*<li><Link to="/login">Login</Link></li>*/}
-        </ul>
+    const authLinks = (
+            <NavbarDropdown>
+        <NavbarDropdown.Toggle className="menu__item">
+        <NavbarDropdown.Open>
+            <i className="fas fa-bars fa-3x" />
+        </NavbarDropdown.Open>
+        <NavbarDropdown.Close>
+            <i className="fa fa-times fa-3x" />
+        </NavbarDropdown.Close>
+        </NavbarDropdown.Toggle>
+        <NavbarDropdown.Menu className="navmenu-menu">
+            <div className="navmenu-menu__row">
+                <Link to='/about-us'>
+                    <NavbarDropdown.Item className="navmenu-item">
+                        <div className="navmenu-item__icon"><i className="fas fa-users" /></div>
+                        <div className="navmenu-item__text">Sobre</div>
+                    </NavbarDropdown.Item>
+                </Link>
+                <Link to='/concerts'>
+                    <NavbarDropdown.Item className="navmenu-item">
+                        <div className="navmenu-item__icon"><i className="fas fa-guitar" /></div>
+                        <div className="navmenu-item__text">Conciertos</div>
+                    </NavbarDropdown.Item>
+                </Link>
+            </div>
+            <div className="navmenu-menu__row">
+                <Link to='/impact'>
+                    <NavbarDropdown.Item className="navmenu-item">
+                        <div className="navmenu-item__icon"><i className="fas fa-hands-helping" /></div>
+                        <div className="navmenu-item__text">Impacto</div>
+                    </NavbarDropdown.Item>
+                </Link>
+            </div>
+            <div className="navmenu-menu__row">
+                <Link to='/contribute'>
+                    <NavbarDropdown.Item className="navmenu-item">
+                        <div className="navmenu-item__icon"><i className="fas fa-donate" /></div>
+                        <div className="navmenu-item__text">Contribuye</div>
+                    </NavbarDropdown.Item>
+                </Link>
+                <Link to='/contact'>
+                    <NavbarDropdown.Item className="navmenu-item">
+                        <div className="navmenu-item__icon"><i className="fas fa-envelope" /></div>
+                        <div className="navmenu-item__text">Contáctanos</div>
+                    </NavbarDropdown.Item>
+                </Link>
+            </div>
+            <div className="navmenu-menu__row">
+                <Link to='/dashboard'>
+                    <NavbarDropdown.Item className="navmenu-item">
+                        <div className="navmenu-item__icon"><i className="fas fa-tachometer-alt" /></div>
+                        <div className="navmenu-item__text">Dashboard</div>
+                    </NavbarDropdown.Item>
+                </Link>
+                <a onClick={logout} href="#!">
+                    <NavbarDropdown.Item className="navmenu-item">
+                        <div className="navmenu-item__icon"><i className="fas fa-sign-out-alt" /></div>
+                        <div className="navmenu-item__text">Logout</div>
+                    </NavbarDropdown.Item>
+                </a>
+            </div>
+        </NavbarDropdown.Menu>
+        </NavbarDropdown>
     );
 
     return (
-        <nav className="navbar bg-light">
-        {   // Change navbar state depending on whether user is auth'd
-            // Can use && because there is a null after :
-          !loading && (
-            <Fragment>{ isAuthenticated ? authLinks : guestLinks }</Fragment>
-        ) }
+        <nav className="navmenu">
+           {!loading && ( <Fragment>{ isAuthenticated ? authLinks : guestLinks }</Fragment>) }
         </nav>
     )
 }
