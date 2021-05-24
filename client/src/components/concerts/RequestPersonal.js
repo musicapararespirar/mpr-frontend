ba01f5584a399f9b9e85562133a7f77c3a1908cb
Evan Thomas <evan@evanjt.com>
2021-05-24 22:54:39 +1000

    Fix time, add fields

--- client/src/components/concerts/RequestPersonal.js
+++ client/src/components/concerts/RequestPersonal.js
@@ -11,6 +11,7 @@ import DatePicker from 'react-datepicker';
 import es from 'date-fns/locale/es';
 import { getProfiles } from '../../actions/profile';
 import ProfileAvailability from '../profile/ProfileAvailability';
+import TimezoneSelect, { i18nTimezones } from 'react-timezone-select'
 
 const RequestPersonal = ({
     requestConcert,
@@ -30,11 +31,21 @@ const RequestPersonal = ({
         }, [getProfiles]);
 
 
+    function roundedDateTime (inDate) {
+        const coeff = 1000 * 60 * 30;
+        const roundedDate = new Date(Math.round(inDate / coeff) * coeff);
+
+        return roundedDate;
+    }
+
+    const [musicianNameEnabled, toggleMusician] = useState(true);
+    const [timePicker, setTime] = useState(roundedDateTime(new Date()));
+    const [profileObject, setProfileObject] = useState('');
     const defaultTimeZone = momentTZ.tz.guess();
     const timeZonesList = momentTZ.tz.names();
     const [formData, setFormData] = useState({
         requesterName: '',
-        requestType: '',
+        requestType: 'personal',
         preferredMusician: false,
         preferredMusicianName: '',
         listenerMessage: '',
@@ -42,7 +53,7 @@ const RequestPersonal = ({
         listenerTimezone: defaultTimeZone,
         listenerNumber: '',
         asap: false,
-        dateFor: moment.now(),
+        dateFor: moment(timePicker).utc(true).format(),
         type: 'personal'
     });
 
@@ -60,10 +71,7 @@ const RequestPersonal = ({
         type
          } = formData;
 
-    const [musicianNameEnabled, toggleMusician] = useState(true);
-    const [timePicker, setTime] = useState(new Date(moment(dateFor)));
-    const [profileObject, setProfileObject] = useState('');
-
+    const [selectedTimezone, setSelectedTimezone] = useState(Intl.DateTimeFormat().resolvedOptions().timeZone)
     const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
 
     if(request !== null && request._id !== null) {
@@ -88,8 +96,57 @@ const RequestPersonal = ({
                 <div className="form-group">
                     <input type="text" placeholder="* Your name" name="requesterName" value={requesterName} onChange={e => onChange(e)} required />
                 </div>
-
                 <div className="form-group">
+                    <input type="radio" id="personal" name="requestType"
+                        value="personal" onChange={e => onChange(e)} checked={requestType==='personal'}/>
+                    <label for="personal">For me</label><br/>
+
+                    <input type="radio" id="gift" name="requestType"
+                        value="gift" onChange={e => onChange(e)}/>
+                    <label for="gift">For someone else</label><br/>
+                </div>
+
+                {requestType === 'gift' ? (
+                    <div className="form-group">
+                    <input type="text" placeholder="* Their name" name="listenerName" value={requesterName} onChange={e => onChange(e)} required />
+                </div>
+                ) : (null)}
+                <div className="form-group">
+                    <input type="text" placeholder="* Your number" name="listenerNumber" value={requesterName} onChange={e => onChange(e)} required />
+                </div>
+                <p>
+                    <input type="checkbox" name="preferredMusician" checked={preferredMusician} value={preferredMusician} onChange={e => {
+                        setFormData({ ...formData, preferredMusician: !preferredMusician });
+                        toggleMusician(!musicianNameEnabled);
+                    }} /> {' '}Request specific musician
+                </p>
+                <div className="form-group">
+                {musicianNameEnabled ? (null) : (
+                <select name="preferredMusicianName"
+                        contentEditable={false}
+                        value={preferredMusicianName}
+                        onChange={ e => { onChange(e) }}
+                        disabled={musicianNameEnabled ? 'disabled' : ''}>
+                        <option value="" selected disabled hidden>Choose Musician</option>
+                        {profiles.map(profile => (
+                            <Fragment>
+                                <option value={profile._id}>{profile.user.name}</option>
+                            </Fragment>))}
+                </select>
+                )}
+                {musicianNameEnabled ? (null) : (profiles.map(profile =>
+                    profile.availability.map(
+                        avail => (profile._id === preferredMusicianName ? (
+                            <Fragment><ProfileAvailability key={avail._id} availability={avail} /></Fragment>) : (<Fragment></Fragment>)))))}
+                </div>
+                <div className="form-group textarea">
+                        <TimezoneSelect
+                        className="form-group dropdown"
+                        value={selectedTimezone}
+                        timezones={{...i18nTimezones}}
+                        onChange={setSelectedTimezone}
+                        />
+
                     <select name="listenerTimezone"
                      value={listenerTimezone}
                      contentEditable={false}
@@ -98,7 +155,7 @@ const RequestPersonal = ({
                         {timeZonesList.map(e => (<Fragment><option value={e}>{e}</option></Fragment>))}
                     </select>
                 </div>
-
+                {console.log(selectedTimezone)}
                 <div className="form-group">
                     <h4>Time requested</h4>
                     <DatePicker
@@ -109,54 +166,14 @@ const RequestPersonal = ({
                               onChange={e => {
                                   setTime(e);
                                   setFormData({ ...formData,
-                                      dateFor: moment.tz(e, listenerTimezone).toISOString()});
+                                      dateFor: moment(e).utc(true).format()});
                               }}/>
                 </div>
-
-                <b>You:</b> Selected time in {defaultTimeZone}: <Moment tz={defaultTimeZone} format="LLLL">{dateFor}</Moment><br/>
-                <b>Them:</b>  Selected time in {listenerTimezone}: <Moment tz={listenerTimezone} format="LLLL">{dateFor}</Moment><br/>
-
                 <div className="form-group">
                 </div>
 
-                <div className="form-group">
-                    <input type="radio" id="gift" name="requestType"
-                        value="gift" onChange={e => onChange(e)}/>
-                    <label for="gift">Gift</label><br/>
 
-                    <input type="radio" id="personal" name="requestType"
-                        value="personal" onChange={e => onChange(e)}/>
-                    <label for="personal">Personal</label><br/>
 
-                    <input type="radio" id="other" name="requestType"
-                        value="other" onChange={e => onChange(e)}/>
-                    <label for="other">Other</label><br/>
-                </div>
-                <p><input type="checkbox" name="preferredMusician" checked={preferredMusician} value={preferredMusician} onChange={e => {
-                    setFormData({ ...formData, preferredMusician: !preferredMusician });
-                    toggleMusician(!musicianNameEnabled);
-                }} /> {' '}Request specific musician</p>
-                <div className="form-group">
-
-                <select name="preferredMusicianName"
-                        contentEditable={false}
-                        onChange={ e => {
-                            onChange(e);
-                        } }
-                        disabled={musicianNameEnabled ? 'disabled' : ''}
-                        >
-                        <option value="" selected disabled hidden>Choose Musician</option>
-                        {profiles.map(profile => (
-                            <Fragment>
-                                <option value={profile._id}>{profile.user.name}</option>
-                            </Fragment>))}
-                </select>
-                {profiles.map(profile =>
-                    profile.availability.map(
-                        avail => (profile._id === preferredMusicianName ? (
-                            <ProfileAvailability key={avail._id} availability={avail} />) : (<Fragment></Fragment>))))}
-
-                </div>
                 <div className="form-group">
                     <input type="text" placeholder="Message for listener" name="listenerMessage" value={listenerMessage} onChange={e => onChange(e)} required />
                 </div>
