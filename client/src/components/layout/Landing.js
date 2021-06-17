86f343a8a21d79822fd94111276ed2e8188ed173
Evan Thomas <evan@evanjt.com>
2021-06-17 22:43:27 +1000

    Replace image of MPR with text

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -22,6 +22,8 @@ import RequestPersonal from '../concerts/RequestPersonal';
 import Gallery from '../information/Gallery';
 import Seasons from '../information/Seasons';
 // import ImageGallery from '../information/ImageGallery';
+import { Textfit } from 'react-textfit';
+
 
 const Landing = ({
     scrollRef,
@@ -49,8 +51,21 @@ const Landing = ({
                 bgImageStyle={{opacity: 0.2, height: '120vh'}}
                 style={{boxShadow: "inset 0px 0px 50px -60px #000000, inset 0px -940px 90px -60px #000000"}}
                 bgImage={slideshow3}>
-                <section className="landing">
-                <img src={logoLarge} className="logo-image"/>
+                <section className="inner-landing-container" style={{
+                    height: '20vw',
+                    width: '30vw',
+                    textAlign: 'center',
+                    display: 'block',
+                    marginTop: '50vh',
+                    }}>
+                    <Textfit mode='single' forceSingleModeWidth={true} max={200}>
+                        <h1 className='mpr-header' style={{ fontFamily: 'sans-serif' }}>
+                            <Translate text='MUSICA' />
+                        </h1></Textfit>
+                        <Textfit mode='single' forceSingleModeWidth={true} max={200}>
+                        <h1 className='mpr-header' style={{ fontFamily: 'sans-serif' }}><Translate text="PARA" /> <Translate text="RESPIRAR" /></h1>
+                    </Textfit>
+                    <Textfit mode='single' forceSingleModeWidth={true} max={200}><h1 className="mpr-header gold" style={{ fontFamily: 'sans-serif' }}><i className='line-gold' style={{marginLeft: 0, marginRight: 0, width: '8rem'}}/>24/7<i className='line-gold' style={{marginLeft: 0, marginRight: 0, width: '8rem'}}/></h1></Textfit>
                 </section>
 
             <div id="request-concert" />
--- client/src/components/layout/PideConcierto.js
+++ client/src/components/layout/PideConcierto.js
@@ -31,18 +31,18 @@ const PideConcierto = ({
             </h1></Textfit>
             <Textfit mode='single' forceSingleModeWidth={true} max={200}>
             <h1 className='mpr-header' style={{ fontFamily: 'sans-serif' }}><Translate text="concierto" /></h1></Textfit>
-                <small style={{
+                <div style={{
                     fontFamily: ' sans-serif',
                     display: 'inline-block',
                     letterSpacing: '1px',
-                    fontSize: '1vw',
+                    fontSize: '0.6rem',
                     lineHeight: 3,
                     textAlign: 'justify',
                     fontWeight: 'lighter'
                 }}>
                 <i className='line-gold' />
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </small><br/>
+                </div><br/>
             <Fragment>
                 <NavbarDropdown>
                     <NavbarDropdown.Toggle className="reqmenu__item">
