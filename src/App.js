b158c0eba7acc2cded7d1bc2a88e71dbfc0f9d7f
Evan Thomas <evan@evanjt.com>
2021-09-18 22:40:48 +1000

    Add umami

--- src/App.js
+++ src/App.js
@@ -10,8 +10,35 @@ import { Provider } from 'react-redux';
 import store from './store';
 import "@fontsource/voltaire";
 import "@fontsource/josefin-sans";
+import { registerUmamiScript } from '@parcellab/react-use-umami'
+import {Helmet} from "react-helmet";
+
 
 const App = () => {
+    useEffect(() => {
+        return (
+        <div className="application">
+            <Helmet>
+                <script async defer data-website-id="8fb3419d-bc6d-4c1b-85a1-36d5d6ab60d4" src="https://umami.evanjt.com/umami.js"></script>
+            </Helmet>
+        </div>
+        )
+        const script = document.createElement('script');
+        script.src = "https://umami.evanjt.com/umami.js";
+        script.async = true;
+        script.defer = true;
+        script["data-website-id"] = "8fb3419d-bc6d-4c1b-85a1-36d5d6ab60d4";
+        console.log(script);
+        const websiteId = "8fb3419d-bc6d-4c1b-85a1-36d5d6ab60d4";
+        const dataDomain = "localhost";
+
+        document.body.appendChild(script);
+
+        return () => {
+            document.body.removeChild(script);
+        }
+
+    }, []);
     return (
     <Provider store={store}>
     <Router>
