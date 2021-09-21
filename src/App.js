cbfe2dd72516c138cac69f121f387063f4fa00ca
Evan Thomas <evan@evanjt.com>
2021-09-21 22:12:59 +1000

    remove script

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ const App = () => {
     <div className="application">
         <Helmet>
             <script async defer data-website-id="a59d1fa2-ade3-4f34-8417-5fdb271798a5" src="https://stats.musicapararespirar.com/umami.js"></script>
-            <script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/18e4b7f031750b28dbf9ff1a5/fc094b98098cbc6dd68d7ab3f.js");</script>
             <link rel="canonical" href="https://musicapararespirar.com" />
         </Helmet>
     </div>
