a19fec7c5b3bde350dc07049cabe7c07cd81133e
Evan Thomas <evan@evanjt.com>
2021-09-24 22:04:49 +1000

    Add newsletter

--- src/components/translation/contact.js
+++ src/components/translation/contact.js
@@ -5,13 +5,43 @@ export default {
         pt: "Fale conosco"
     },
     "newsAndUpdates": {
-        en: "For news and updates you can follow our social media channels on",
+        en: "For news and updates you can follow us on",
         es: "Para más noticias puedes seguirnos en",
         pt: "Para mais notícias você pode nos seguir no"
+    },
+    "andmailinglist": {
+        en: "and also join our mailing list",
+        es: "",
+        pt: ""
     },
     "and": {
         en: "and",
         es: "y",
         pt: "e"
+    },
+    "* {firstName}": {
+        en: "* First name",
+        es: "* Nombre",
+        pt: "* Nome"
+    },
+    "* {lastName}": {
+        en: "* Last name",
+        es: "* Apellido",
+        pt: "* Sobrenome"
+    },
+    "* {email}": {
+        en: "* Email",
+        es: "* Dirección de correo",
+        pt: "* Email"
+    },
+    "languagePref": {
+        en: "* Language preference",
+        es: "* Idioma de preferencia",
+        pt: "* Idioma preferido"
+    },
+    "SEND": {
+        en: "SEND",
+        es: "ENVIAR",
+        pt: "MANDAR"
     }
 }
